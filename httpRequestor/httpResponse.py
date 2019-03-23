4a84e4f4473d46fe2c0cef3c1460c844e520d73b
Yashwardhan Pawar <code.volunteer18@gmail.com>
2019-03-23 05:39:55 -0700

    (+) File Utility and Google scrapper

---
+++ httpRequestor/httpResponse.py
@@ -0,0 +1,72 @@
+'''
+Created on Feb 21, 2019
+
+@author: code
+'''
+
+import requests
+import json
+
+
+class httpResponseClass(object):
+    '''
+    classdocs
+    '''
+    
+    def getResponse(self):
+        # sending get request and saving the response as response object 
+        if self.PARAMS is not None:
+            self.responseReturn = requests.get(url=self.URL, params=self.PARAMS).content
+            self.isJSONFlag = self.JSONCheck()
+            return self.responseReturn 
+        else:
+            self.responseReturn = requests.get(url=self.URL).content
+            self.isJSONFlag = self.JSONCheck()
+            return self.responseReturn 
+        # extracting data in json format 
+    
+    def isJSON(self):
+        self.responseReturn = self.getResponse()
+        return self.isJSONFlag
+    
+    def getJSONResponse(self):
+        if (self.isJSON()):
+            return self.JSONResponse
+        else:
+            return self.JSONResponse
+        
+    def JSONCheck(self):
+        if(isinstance(self.responseReturn, str)):
+            try:
+                self.JSONResponse = json.loads(self.responseReturn)
+                return True
+            except:
+                return False
+        else:
+            # print("Response Type: " + str(type(self.responseReturn)))
+            return False    
+   
+    def getResponseCode(self):
+        return requests.get(self.URL, self.PARAMS).status_code
+    
+    def isRedirect(self):
+        return requests.get(self.URL, self.PARAMS)
+  
+#     def getJSONResponse(self):
+#         # sending get request and saving the response as response object 
+#         r = requests.get(url = self.URL, params = self.PARAMS) 
+#         # extracting data in json format 
+#         return r.json() 
+
+    def paramParser(self):
+        print(type(self.PARAMS))
+
+    def __init__(self, URL, PARAMS=None):
+        '''
+        Constructor
+        '''
+        self.URL = URL
+        self.PARAMS = PARAMS
+        self.responseReturn = None
+        self.isJSONFlag = False
+        self.JSONResponse = json.loads('{"status": "Invalid Request: Return value is not JSON format"}')
--- utilityPackage/IntegerUtility.py
+++ utilityPackage/IntegerUtility.py
@@ -4,47 +4,36 @@ Created on Feb 15, 2019
 @author: code
 '''
 
-class IntUtility(object):
-    '''
-    classdocs
-    '''
-    
-    def isIntOrFloat(self, testObject):
-        if isinstance(testObject, int):
+def isIntOrFloat(self, testObject):
+    if isinstance(testObject, int):
+        return True
+    else:
+        try:
+            int(testObject)
             return True
-        else:
-            try:
-                int(testObject)
-                return True
-            except:
-                try:
-                    float(testObject)
-                    return True
-                except:
-                    return False
-
-    def isInt(self, testObject):
-        if isinstance(testObject, int):
-            return True
-        else:
-            try:
-                int(testObject)
-                return True
-            except:
-                return False
-
-    def isFloat(self, testObject):
-        if isinstance(testObject, float):
-            return True
-        else:
+        except:
             try:
                 float(testObject)
                 return True
             except:
                 return False
 
+def isInt(self, testObject):
+    if isinstance(testObject, int):
+        return True
+    else:
+        try:
+            int(testObject)
+            return True
+        except:
+            return False
 
-    def __init__(self):
-        '''
-        Constructor
-        '''
+def isFloat(self, testObject):
+    if isinstance(testObject, float):
+        return True
+    else:
+        try:
+            float(testObject)
+            return True
+        except:
+            return False
